344c80daf0e31db7c7d69b1a1d8c51a9458a613d
DYazhog <halperae@gmail.com>
2016-07-01 18:37:47 +0000

    Create main.java

---
+++ training/main.java
@@ -0,0 +1,24 @@
+
+
+import static face.jan.ara;
+import static face.jan.run;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import org.opencv.core.Core;
+
+/**
+ *
+ * @author ANASS karim
+ */
+
+public class main {
+public static void main(String[] args) throws IOException {
+System.out.println(System.getProperty("java.library.path"));
+System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+new  training(). trainning ();
+ 
+  }
+}
